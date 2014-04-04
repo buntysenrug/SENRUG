16b19d13953316b9025b46145e86626ac3d770ee
Bunty <bunty.newcastle@gmail.com>
2014-04-04 01:58:53 +0100

    Installed Parse SDK
    Installed and as well as checked Parse SDK

--- src/com/packagemain/senrug/AlternateRoute.java
+++ src/com/packagemain/senrug/AlternateRoute.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
 
 public class AlternateRoute extends Activity{
 	WebView viewa;
@@ -11,6 +14,11 @@ public class AlternateRoute extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		Parse.initialize(this, "Nr5seKfxX76kVAv4d64gKGsPR63gZGt8eSPjuonO", "XIYlZvOczar9HFXAiRiNoETH4EREib4MDm0ab3rH");
+		ParseObject station=new ParseObject("Station");
+		station.put("name", "Morpeth");
+		station.saveInBackground();
+		
 		setContentView(R.layout.alternate_route);
 		
 		String url="http://jplanner.travelinenortheast.info/";
