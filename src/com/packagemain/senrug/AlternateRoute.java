24a42a798df4021991c6b0d29b10f38dd57707b0
Bunty <bunty.newcastle@gmail.com>
2014-04-16 14:41:33 +0100

    Nearest Station working Correctly
    Locate Station working correctly

--- src/com/packagemain/senrug/AlternateRoute.java
+++ src/com/packagemain/senrug/AlternateRoute.java
@@ -2,10 +2,16 @@ package com.packagemain.senrug;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.parse.LocationCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 
@@ -16,13 +22,16 @@ public class AlternateRoute extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		Parse.initialize(this, "Nr5seKfxX76kVAv4d64gKGsPR63gZGt8eSPjuonO", "XIYlZvOczar9HFXAiRiNoETH4EREib4MDm0ab3rH");
+		
 		/*ParseObject station=new ParseObject("Station");
 		station.put("name", "Haymarket");
 		ParseGeoPoint p=new ParseGeoPoint(40,-30);
+		LatLng l=new LatLng(p.getLatitude(),p.getLongitude());
 		station.put("location",p);
 		station.saveInBackground();
 		*/
 		setContentView(R.layout.alternate_route);
+		
 		
 		String url="http://jplanner.travelinenortheast.info/";
 		viewa=(WebView) findViewById(R.id.webviewa);
--- src/com/packagemain/senrug/LocateActivity.java
+++ src/com/packagemain/senrug/LocateActivity.java
@@ -1,5 +1,7 @@
 package com.packagemain.senrug;
 
+import java.util.List;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -7,9 +9,22 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.parse.FindCallback;
+import com.parse.LocationCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class LocateActivity extends Activity{
 	
@@ -20,9 +35,10 @@ public class LocateActivity extends Activity{
 	  protected void onCreate(Bundle savedInstanceState) {
 		    super.onCreate(savedInstanceState);
 		    setContentView(R.layout.map);
-		    
-		    map=((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
-		    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+			Parse.initialize(this, "Nr5seKfxX76kVAv4d64gKGsPR63gZGt8eSPjuonO", "XIYlZvOczar9HFXAiRiNoETH4EREib4MDm0ab3rH");
+
+		  map=((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+		  /*  Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
 		        .title("Hamburg"));
 		    Marker kiel = map.addMarker(new MarkerOptions()
 		        .position(KIEL)
@@ -30,12 +46,49 @@ public class LocateActivity extends Activity{
 		        .snippet("Kiel is cool")
 		        .icon(BitmapDescriptorFactory
 		            .fromResource(R.drawable.ic_launcher)));
+		    
+		   
 
 		    // Move the camera instantly to hamburg with a zoom of 15.
 		    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
 
 		    // Zoom in, animating the camera.
 		    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		    */
+		 final ProgressDialog dialog = ProgressDialog.show(this, "", "Looking for Nearest SENRUG Station......", true);
+		  dialog.show();
+			GPS gps=new GPS(this);
+			//static final lat=gps.getLatitude()
+			ParseGeoPoint p=new ParseGeoPoint(gps.getLatitude(),gps.getLongitude());
+			
+			ParseQuery<ParseObject> query = ParseQuery.getQuery("Station");
+			query.whereNear("location", p);
+			
+			query.setLimit(10);
+			query.findInBackground(new FindCallback<ParseObject>(){
+
+				@Override
+				public void done(List<ParseObject> objects, ParseException e) {
+					// TODO Auto-generated method stub
+					String s=(String) objects.get(0).get("name");
+					ParseGeoPoint g=objects.get(0).getParseGeoPoint("location");
+					LatLng p=new LatLng(g.getLatitude(),g.getLongitude());
+					Toast.makeText(getApplicationContext(), s, s.length()).show();
+					dialog.dismiss();
+					Marker nearest=map.addMarker(new MarkerOptions().position(p).title(s));
+				    map.moveCamera(CameraUpdateFactory.newLatLngZoom(p, 15));
+				    //map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+				   // Polyline line=map.addPolyline(new PolylineOptions().add((p),new LatLng(0,0)));
+
+				    
+
+					
+				}
+				
+			});
+
+
+			
 		  }
 
 	
